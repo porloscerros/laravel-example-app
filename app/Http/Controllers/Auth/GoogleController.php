3f25cfe918e6e2df458884178784607f56238c89
porloscerros <porloscerros@gmail.com>
2024-03-04 00:09:21 -0300

    fix: code style.

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -10,10 +10,10 @@ use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController
 {
-
     public function redirect()
     {
         $parameters = ['access_type' => 'offline'];
+
         return Socialite::driver('google')
             ->scopes(config('google_apis.scopes'))
             ->with($parameters)
@@ -24,18 +24,18 @@ class GoogleController
     {
         $googleUser = Socialite::driver('google')->user();
 
-        if($current_user = User::where('google_id', $googleUser->id)->first()) {
+        if ($current_user = User::where('google_id', $googleUser->id)->first()) {
             Auth::login($current_user);
+
             return redirect()->intended('dashboard');
         }
 
-        if($user = User::where('email', $googleUser->email)->first()) {
+        if ($user = User::where('email', $googleUser->email)->first()) {
             $user->update([
                 'google_id' => $googleUser->id,
                 'google_refresh_token' => $googleUser->refreshToken,
             ]);
-        }
-        else {
+        } else {
             $user = User::create([
                 'name' => $googleUser->name,
                 'email' => $googleUser->email,
